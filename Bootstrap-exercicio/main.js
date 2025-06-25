c67619c13f275d2819658afd6dc1e038720f6b9a
VIctor-teles-Dev <victoreduardoteleschaves@gmail.com>
2025-06-25 19:44:27 -0300

    adicionando a atividade de Bootstrap

---
+++ Bootstrap-exercicio/main.js
@@ -0,0 +1,24 @@
+// Aplica máscara ao campo de telefone usando o plugin jQuery Mask
+$(document).ready(function(){
+    $('#telefone').mask('(00) 00000-0000');
+});
+
+// Script de validação de formulário do Bootstrap 5
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
